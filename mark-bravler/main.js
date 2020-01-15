73be47d916245666b4fd3f012a2aa7300eee6d76
itschoolgomother <itschoolgomother>
2020-01-15 13:48:27 +0200

    15.1.2020

---
+++ mark-bravler/main.js
@@ -0,0 +1,8 @@
+
+var test = prompt("Вопрос ?Гони бабло!","132"); 
+var usd = 24;
+ alert(Math.round(test/usd)+"usd");
+var cfh = 24.69;
+ alert(Math.round(test/cfh)+"cfh");
+ var bitcoin = 193 310.15;
+ alert(Math.round(test/usd)+"bitcoin")
