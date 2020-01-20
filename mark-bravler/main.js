cbcf5e7bcc566940f7825b73fe91161261334437
itschoolgomother <itschoolgomother>
2020-01-20 17:29:45 +0200

    20.1.2020

--- mark-bravler/main.js
+++ mark-bravler/main.js
@@ -1,8 +1,14 @@
 
-var test = prompt("Вопрос ?Гони бабло!","132"); 
-var usd = 24;
- alert(Math.round(test/usd)+"usd");
-var cfh = 24.69;
- alert(Math.round(test/cfh)+"cfh");
- var bitcoin = 193 310.15;
- alert(Math.round(test/usd)+"bitcoin")
+// var test = prompt("Вопрос ?Гони бабло!","132"); 
+// var usd = 24;
+//  alert(Math.round(test/usd)+"usd");
+// var cfh = 24.69;
+//  alert(Math.round(test/cfh)+"cfh");
+//  var bitcoin = 193 310.15;
+//  alert(Math.round(test/usd)+"bitcoin")
+var box = document.getElementById('box')
+var size = prompt("size","100")
+var color = prompt("blue");
+box.style.width = size + "px";
+box.style.height = size + "px";
+box.style.backgroundColor = color;
